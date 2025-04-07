3f84e0373a8afc0aa38835bb5af40a45553bd752
Kassandra <kassandraiatrou@gmail.com>
2025-04-07 06:38:40 +0000

    task 1 inserts a node as the left-child of another node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,6 +12,11 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
+
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
 	/*Using previous function to create new node*/
 	new_node = binary_tree_node(parent, value);
 	/*Set the left pointer of new node to current left child of parent*/
